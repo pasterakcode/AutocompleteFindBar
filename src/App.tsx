c83d9124dbff5eedaf6d86e74e85d5205dfe1f71
pasterakcode <pasterakcode@gmail.com>
2022-01-16 00:28:26 +0100

    selected (mouse e) and add new skill: ready

--- src/App.tsx
+++ src/App.tsx
@@ -29,42 +29,51 @@ function App() {
 	};
 
 	const handleDeleteSkill = (skillToDelete: string): void => {
-		const deletedSkill = skills?.find((skill: Skill): boolean => skill.name === skillToDelete) || [];
-		console.log(deletedSkill)
-		setSkillsToShow(prev => ([...prev].concat(deletedSkill)).sort((a,b) => a.skill_id > b.skill_id? 1 : -1));
+		const deletedSkill =
+			skills?.find((skill: Skill): boolean => skill.name === skillToDelete) ||
+			[];
+		setSkillsToShow(prev =>
+			[...prev]
+				.concat(deletedSkill)
+				.sort((a, b) => (a.skill_id > b.skill_id ? 1 : -1))
+		);
 		setSelectedSkills(prev => prev.filter(skill => skill !== skillToDelete));
 	};
 
 	const handleFindSkillsInput = (charsToFind: string): void => {
-		const findedSkill =
+		setSkillsToShow(
 			skills?.filter(
-				(skill: Skill): boolean =>
+				skill =>
 					skill.name.toUpperCase().includes(charsToFind.toUpperCase()) &&
 					!selectedSkills.includes(skill.name)
-			) || [];
-		setSkillsToShow(findedSkill);
+			)
+		);
 	};
 
 	return (
 		<div className='App'>
 			skill
 			<FindBar
+				firstSkillFromShow={skillsToShow[0]}
 				selectedSkills={selectedSkills}
 				onHandleFindSkillsInput={handleFindSkillsInput}
 				onHandleDeleteSkill={handleDeleteSkill}
+				onHandleSelectSkill={handleSelectSkill}
 			/>
 			<ul>
-			{skillsToShow ? (
-				skillsToShow.sort().map(skill => (
-					<OneBarSkills
-						key={skill.skill_id}
-						skill={skill}
-						onHandleSelectSkill={handleSelectSkill}
-					/>
-				))
-			) : (
-				<div>wczytuje strone</div>
-			)}
+				{skillsToShow ? (
+					skillsToShow
+						.sort()
+						.map(skill => (
+							<OneBarSkills
+								key={skill.skill_id}
+								skill={skill}
+								onHandleSelectSkill={handleSelectSkill}
+							/>
+						))
+				) : (
+					<div>wczytuje strone</div>
+				)}
 			</ul>
 		</div>
 	);
--- src/components/findBar/FindBar.tsx
+++ src/components/findBar/FindBar.tsx
@@ -1,27 +1,52 @@
-import { Skill } from "../../interface/Interface";
+import { Skill } from '../../interface/Interface';
 import { useState, useEffect } from 'react';
-import { toEditorSettings } from "typescript";
-import OneSelectedSkill from "./oneSelectedSkill/OneSelectedSkill";
+import { toEditorSettings } from 'typescript';
+import OneSelectedSkill from './oneSelectedSkill/OneSelectedSkill';
 
 interface FindBarProps {
-    selectedSkills: string[],
-    onHandleFindSkillsInput: Function,
-    onHandleDeleteSkill: Function,
+    firstSkillFromShow: Skill;
+	selectedSkills: string[];
+	onHandleFindSkillsInput: Function;
+	onHandleDeleteSkill: Function;
+	onHandleSelectSkill: Function;
 }
 
-const FindBar = ( { selectedSkills, onHandleFindSkillsInput, onHandleDeleteSkill }: FindBarProps ) => {
-    const [inputValue, setInputValue] = useState<string>()
+const FindBar = ({
+    firstSkillFromShow,
+	selectedSkills,
+	onHandleFindSkillsInput,
+	onHandleDeleteSkill,
+	onHandleSelectSkill,
+}: FindBarProps) => {
+	const [inputValue, setInputValue] = useState<string>();
 
-    const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        const toFind = e.target.value;
-        setInputValue(toFind);
-        onHandleFindSkillsInput(toFind)
+	const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
+		const toFind = e.target.value;
+		setInputValue(toFind);
+		onHandleFindSkillsInput(toFind);
+	};
+    const addCustomSkill = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+        if (e.code === "Enter") {
+            onHandleSelectSkill(inputValue)
+        }
     }
-    return (
-        <div>
-            {selectedSkills?.map( ( skill: string, index: number ) => <OneSelectedSkill key={index} skillName={skill} onHandleDeleteSkill={onHandleDeleteSkill}/>)}
-            <input type="text" placeholder="find..." value={inputValue} onChange={e => handleChangeInput(e)}/>
-        </div>
-    )
-}
+	return (
+		<div>
+			{selectedSkills?.map((skill: string, index: number) => (
+				<OneSelectedSkill
+					key={index}
+					skillName={skill}
+					onHandleDeleteSkill={onHandleDeleteSkill}
+				/>
+			))}
+			<input
+				type='text'
+				placeholder='find...'
+				value={inputValue}
+				onChange={e => handleChangeInput(e)}
+                onKeyPress={e => addCustomSkill(e)}
+			/>
+		</div>
+	);
+};
 export default FindBar;
